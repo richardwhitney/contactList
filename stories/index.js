254a72f430dd3d5cf7c38fd17cfecc5548022a4f
Richard Whitney <rwhitney32@gmail.com>
2019-06-26 22:09:36 +0100

    Header component completed

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Header from '../src/components/header';
+
+storiesOf("Contact List App/Header", module).add("default", () =>(
+  <Header noContacts={10}/>
+));
