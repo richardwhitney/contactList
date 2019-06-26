4727e9b8181ceee36486782e85fbd5f86f766698
Richard Whitney <rwhitney32@gmail.com>
2019-06-26 23:08:55 +0100

    ContactList component completed

---
+++ src/components/contactList/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Contact from "../contact/";
+import './contactList.css';
+
+export default class ContactList extends Component {
+  render() {
+    const contactCards = this.props.contacts.map(c => (
+      <Contact key={c.phone} contact={c}/>
+    ));
+    return (
+      <div className="container-fluid contacts bg-info">
+        <div className="row">{contactCards}</div>
+      </div>
+    );
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Header from '../src/components/header';
 import FilterControls from '../src/components/filterControls';
 import Contact from '../src/components/contact/';
+import ContactList from '../src/components/contactList/';
 
 const sample = {
   name: {first:'Joe', last:'Bloggs'},
@@ -23,3 +24,8 @@ storiesOf("Contact List App/Filter Controls", module).add("default", () => (
 storiesOf("Contact List App/Contact", module).add("default", () => (
   <Contact contact={sample}/>
 ));
+
+storiesOf("Contact List App/Contact List", module).add("default", () => {
+  const samples = [sample, sample, sample, sample, sample];
+  return <ContactList contacts={samples}/>
+});
