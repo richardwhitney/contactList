d03b870bb695114bc23b18a0aa2a02ffbcdc2056
Richard Whitney <rwhitney32@gmail.com>
2019-06-26 23:11:03 +0100

    Static version completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header/";
+import ContactList from "./components/contactList/";
+import FilterControls from "./components/filterControls/";
 
 class App extends Component {
   render() {
+    const sample = {
+      name: { first: "Joe", last: "Bloggs" },
+      email: "j.bloggs@example.com",
+      phone: "012-3456789",
+      picture: { thumbnail: "./profile.png" }
+    };
+
+    const contacts = [sample, sample, sample, sample, sample];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="jumbotron">
+        <Header noContacts={10} />
+        <FilterControls />
+        <ContactList contacts={contacts} />
       </div>
     );
   }
