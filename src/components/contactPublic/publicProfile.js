65c602512dda2fc3d94614078594e98ecaa04a29
Richard Whitney <rwhitney32@gmail.com>
2019-07-08 21:04:57 +0100

    PublicProfile component completed

---
+++ src/components/contactPublic/publicProfile.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+//import { capitalize } from "../../util";
+import "../../fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default ({ user }) => {
+  const address = `${user.location.street}, ${user.location.city}, ${user.location.state}`;
+  const dob = user.dob.date.split("-");
+  const birthdate = `${dob[2].substr(0, 2)}/${dob[1]}/${dob[0]}  `;
+  return (
+    <Fragment>
+      <h4>
+        <FontAwesomeIcon icon={["fas", "home"]} />
+        <span> {address}</span>
+      </h4>
+      <h4>
+        <FontAwesomeIcon icon={["fas", "phone"]} />
+        <span> {user.phone}</span>
+      </h4>
+      <h4>
+        <FontAwesomeIcon icon={["fas", "envelope"]} />
+        <span> {user.email}</span>
+      </h4>
+      <h4>
+        <FontAwesomeIcon icon={["fas", "birthday-cake"]} />
+        <span> {birthdate}</span>
+      </h4>
+    </Fragment>
+  );
+};
--- stories/index.js
+++ stories/index.js
@@ -6,12 +6,59 @@ import FilterControls from '../src/components/filterControls';
 import Contact from '../src/components/contact/';
 import ContactList from '../src/components/contactList/';
 import { action } from "@storybook/addon-actions";
+import PublicProfile from '../src/components/contactPublic/publicProfile';
 
 const sample = {
-  name: {first:'Joe', last:'Bloggs'},
-  email: 'j.bloggs@example.com',
-  phone: '012-3456789',
-  picture: {thumbnail: './profile.png'}
+  gender: "female",
+  name: {
+    title: "mrs",
+    first: "sofia",
+    last: "gagné"
+  },
+  location: {
+    street: "4459 22nd ave",
+    city: "delisle",
+    state: "yukon",
+    postcode: "M3O 5T8",
+    coordinates: {
+      latitude: "-65.3384",
+      longitude: "5.7232"
+    },
+    timezone: {
+      offset: "-3:00",
+      description: "Brazil, Buenos Aires, Georgetown"
+    }
+  },
+  email: "sofia.gagné@example.com",
+  login: {
+    uuid: "e575704f-7425-4a46-9351-1c8883df8c5a",
+    username: "bigbutterfly807",
+    password: "franks",
+    salt: "sitQqAvp",
+    md5: "6ba3796b4ada40022882d952ec2b69c4",
+    sha1: "1692278da10018f7f400dbf6faeee74b39fce8ec",
+    sha256: "1e3b7af5552ed8436357e5a46dd78ca40673da8d2f4b9450808f6f73b7236dfa"
+  },
+  dob: {
+    date: "1973-02-09T08:49:19Z",
+    age: 46
+  },
+  registered: {
+    date: "2010-11-07T18:55:04Z",
+    age: 8
+  },
+  phone: "289-583-6562",
+  cell: "223-771-0858",
+  id: {
+    name: "",
+    value: null
+  },
+  picture: {
+    large: "https://randomuser.me/api/portraits/women/49.jpg",
+    medium: "https://randomuser.me/api/portraits/med/women/49.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/women/49.jpg"
+  },
+  nat: "CA"
 }
 
 storiesOf("Contact List App/Header", module).add("default", () =>(
@@ -30,3 +77,7 @@ storiesOf("Contact List App/Contact List", module).add("default", () => {
   const samples = [sample, sample, sample, sample, sample];
   return <ContactList contacts={samples}/>
 });
+
+storiesOf("Contact List App/Contact Page/PublicProfile", module).add("default", () => (
+  <PublicProfile user={sample}/>
+));
