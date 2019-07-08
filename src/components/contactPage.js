a58a3ced8c7c56300ad9b1033bcb2358b726bb99
Richard Whitney <rwhitney32@gmail.com>
2019-07-08 22:27:54 +0100

    Contact page without Map working

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -1,11 +1,22 @@
 import React, {Fragment} from "react";
 import {withRouter} from "react-router-dom";
+import api from "../dataStore/stubAPI";
+import ContactPublic from "../components/contactPublic/";
+import ContactPrivate from "../components/contactPrivate";
 
 const ContactPage = props => {
+  const { id } = props.match.params;
+  const contact = api.find(id);
   return (
     <Fragment>
-      <h3>Contact id: {props.match.params.id}</h3>
-      <h3>Detail page stub</h3>
+      { contact ? (
+        <Fragment>
+          <ContactPublic user={contact}/>
+          <ContactPrivate user={contact}/>
+        </Fragment>
+      ) : (
+        <p>Waiting for contact details</p>
+      )}
     </Fragment>
   );
 };
